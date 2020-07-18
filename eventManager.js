fff72a0452bece75bacbae35f52eaae384e6244d
Patrick Shim <shimp@oregonstate.edu>
2020-07-17 22:41:43 -0700

    adding files

---
+++ eventManager.js
@@ -0,0 +1,37 @@
+
+var express = require('express');
+var path = require('path');
+var app = express();
+
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 1111);
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.use(function(req,res){
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function(){
+  console.log(
+    `Express started on http://${process.env.HOSTNAME}:${app.get(
+      'port'
+    )}; press Ctrl-C to terminate.`
+  );
+});
