23fe98e09e17ad8e8050ecebaec31f8eecb596cc
Patrick Shim <shimp@oregonstate.edu>
2020-07-26 16:10:33 -0700

    Added descriptions for button functions

--- eventManager.js
+++ eventManager.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 11111);
+app.set('port', 1111);
 
 app.get('/home', (req, res) => {
   res.render('home');
