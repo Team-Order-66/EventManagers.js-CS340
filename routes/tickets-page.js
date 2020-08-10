10d0f93a81c63fa203fac545d3e70b0036b3a14d
Kiwon Nam <kiwonnam2016@u.northwestern.edu>
2020-08-09 22:36:45 -0500

    minor changes to tickets page and tickets route to complete on different computer.

--- routes/tickets-page.js
+++ routes/tickets-page.js
@@ -1,35 +1,23 @@
-// Backend for our Tickets page
+/**
+ *  Tickets Page - routes
+ *  SELECT - Based on search via the name of the Event
+ *  INSERT - B
+ */
 
-const express = require('express');
-const router = express.Router();
 
 
-// RENDERS TICKET PAGE -- query for user yet to properly be impelemented
-router.get('/tickets', function(res, req){
-    var context = {};
-    var sqlQuery = `
-    SELECT
-	    Event.eventName as Event,
-        Event.date as Date,
-        Event.ticketPrice as Price
-    FROM
-	    Event
-    INNER JOIN GuestList 
-	    USING (eventID)
-    WHERE
-        GuestList.customerID=?`
-    
-        mysql.pool.query(sqlQuerey, [req.query.customer_id], function(err, result){
-            if(err){
-                next(err);
-                return;
-              } else{
-                  context.event = result;
-                  res.render('/tickets',context);
-              }
+module.exports = function() {
+    const express = require('express');
+    const router = express.Router();
 
-        })
-        
-})
+    //function that will be used to get all events that are currently in the database
+    function getAllEvents(res, mysql, context, complete) {
+        let sqlQuery = "SELECT ";
 
-module.exports = router;
+
+    }
+
+
+    // function that gets the events based on the name of the event
+    function searchEvents(res, mysql)
+}
