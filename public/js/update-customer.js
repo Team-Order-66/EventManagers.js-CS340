1ff913634804d02aa98f1b8c8b1942ee9237e840
KiwonNam2016 <kiwonnam2016@u.northwestern.edu>
2020-08-13 03:27:07 -0500

    fixed update

--- public/js/update-customer.js
+++ public/js/update-customer.js
@@ -2,11 +2,12 @@
 
 function updateCustomer(customerID){
 	$.ajax({
-		url: '/customers/' + customerID,
+		url: '/customers/' + customerID, 
 		type: 'PUT',
-		data: $('#customer-update').serialize(), 
+		data: $('#customer-update').serialize(), // serialize the data we got from the inputs
 		success: function(result){
-		    window.location.replace("./");  // Navigate to previous 
+            // after succesfully updating, we will go to the url /customers using the .assign- essentially back to our previous page
+		    window.location.assign("/customers");  
 		}
 	})
 };
